5dccb4d63dc2c32b3f565e85ac82a2b60db36232
Farahsaiza <farahsaiza@gmail.com>
2023-10-19 22:02:09 +0100

    Add files via upload

---
+++ month days.py
@@ -0,0 +1,28 @@
+month=str(input("entre a month :"))
+match month :
+    case "January":
+        print("31 days")
+    case "February":
+        print("28 days or 29")
+    case "March":
+        print("31  days")
+    case "April":
+        print("30  days")
+    case "May":
+        print("31  days")
+    case "June":
+        print("30  days")
+    case "July":
+        print("31  days")
+    case "August":
+        print("31  days")
+    case "September":
+        print("30  days")
+    case "October":
+        print("31  days")
+    case "November":
+        print("30  days")
+    case "december":
+        print("31  days")
+
+        
